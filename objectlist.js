cb856f5d60bad1b23031f65707c393cbc54f8ba7
Deepti-20 <80585331+Deepti-20@users.noreply.github.com>
2021-12-08 00:49:17 +0530

    Add files via upload

---
+++ objectlist.js
@@ -0,0 +1,9 @@
+var questionOne = {
+    question: "Who is my favorite superhero?"
+    answer: "Dhruv"
+  }
+  
+  questionTwo = {
+    question: "Which is my favorite sad song?"
+    answer: "Channa Meraya"
+  }
