88442adaf9d164bc886a07c176e5e839b695dbb5
Deepti-20 <80585331+Deepti-20@users.noreply.github.com>
2021-12-08 00:53:39 +0530

    Add files via upload

---
+++ listQuestions.js
@@ -0,0 +1,12 @@
+var arr1 = ["milk","bread","jam","butter","coffee","sugar","tea leaves","namkeen"];
+var listLength=arr1.length
+console.log(arr1[0])
+console.log(arr1[2])
+console.log(arr1[arr1.length-1],"\n")
+
+var grocery=["dry fruits","daal","rice","floor","poha"];
+var listLength=grocery.length
+for (a = 0; a < listLength; a=a+1)
+{   
+    console.log(grocery[a])
+}
