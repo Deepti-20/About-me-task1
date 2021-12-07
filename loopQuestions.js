f09d534a6f21db87b255795e6bf94919f16ae776
Deepti-20 <80585331+Deepti-20@users.noreply.github.com>
2021-12-08 00:50:59 +0530

    Add files via upload

---
+++ loopQuestions.js
@@ -0,0 +1,14 @@
+for(var i = 0; i < 5; i++){
+    console.log("Deepti chauhan")
+ }
+ 
+ let c=5;
+ let string="";
+ for (let i = 1; i <= c; i++){
+     for (let j = 1; j <= i; j++){
+         string+="*"
+     }
+     string+="\n"
+ }
+ console.log(string);
+ 
